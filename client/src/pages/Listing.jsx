b10ecea4fd7169a2ca04a118c5918c29bff93670
selam <selblessed@gmail.com>
2024-05-09 10:08:22 -0400

    fix import errors

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide} from 'swiper/react';
-import SwiperCore from 'Swiper';
+import SwiperCore from 'swiper';
 import { useSelector } from 'react-redux';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
